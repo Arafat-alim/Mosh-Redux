eccba25d12500d0e770de3523d1218b52a0de047
Arafat <arafat97alim@gmail.com>
2021-08-09 00:39:28 +0530

    Dispatch codes added and working fine

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import store from "./store";
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
 
-console.log(store);
+store.dispatch({
+  type: "bugsRemoved",
+  payload: {
+    id: 1,
+  },
+});
+console.log(store.getState());
