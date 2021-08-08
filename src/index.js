a38be984afd3fb3bf5ade8c71e267b6d6ea3f4fc
Arafat <arafat97alim@gmail.com>
2021-08-09 01:04:20 +0530

    subscribe and unsubscribe added.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import store from "./store";
+
+let unsubscribe = store.subscribe(() => {
+  console.log("Store changed", store.getState());
+});
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "Bug1",
   },
 });
+
+unsubscribe(); // ye jaise add hoga waise subscribe track krna bandh kr dega
 
 store.dispatch({
   type: "bugsRemoved",
