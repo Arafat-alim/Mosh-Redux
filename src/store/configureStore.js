8c2c0277317e03cbcadec182af4fb822b38db93a
Arafat <arafat97alim@gmail.com>
2021-08-10 20:20:09 +0530

    configuration our store using reduxToolkit.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,17 @@
 import reducer from "./bugs";
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { devToolsEnhancer } from "redux-devtools-extension";
 
-export default function configureStore() {
-  let store = createStore(
-    reducer,
-    //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //now we dont need
-    devToolsEnhancer({ trace: true })
-  );
+// export default function configureStore() {
+//   let store = createStore(
+//     reducer,
+//     //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //now we dont need
+//     devToolsEnhancer({ trace: true })
+//   );
+//   return store;
+// }
 
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
